610835a0c82bd1482d3c866671433b5bae7d4600
Tenium <amin.behravan@yahoo.com>
2026-06-15 19:50:33 +0330

    Create App.xaml.cs

---
+++ src/App.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows;
+
+namespace LANSpark
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // در صورت نیاز، کدهای آماده‌سازی اولیه قبل از باز شدن فرم در اینجا قرار می‌گیرند.
+        }
+    }
+}
